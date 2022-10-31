68356fec4fca03549fd987a37c247e697ff5dd8b
Chengyuan Cai <cc4fy@virginia.edu>
2022-10-31 13:33:44 -0400

    add google map api to app
    - added google maps to app

---
+++ GoogleMaps/app/src/main/java/com/example/googlemaps/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.googlemaps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+}
